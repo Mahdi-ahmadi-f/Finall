18f64f7dccea5690eaecf70248613ebcf6640646
Mhmmd <ahmadi.mohammad9776@gmail.com>
2025-02-23 23:16:56 +0330

    Add files via upload

---
+++ Final_Project_Dev_Ahmadi/Final_Project_Dev/CSV_FORM/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace CSV_FORM
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button2 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            groupBox1 = new GroupBox();
+            button1 = new Button();
+            label2 = new Label();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            groupBox2 = new GroupBox();
+            textBox2 = new TextBox();
+            groupBox3 = new GroupBox();
+            textBox3 = new TextBox();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.LimeGreen;
+            button2.Cursor = Cursors.Hand;
+            button2.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(20, 56);
+            button2.Name = "button2";
+            button2.Size = new Size(108, 34);
+            button2.TabIndex = 1;
+            button2.Text = "Logging on File";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(16, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(108, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Next Logging Time";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(147, 25);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(91, 23);
+            textBox1.TabIndex = 3;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(254, 105);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Sampling";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.LimeGreen;
+            button1.Location = new Point(20, 56);
+            button1.Name = "button1";
+            button1.Size = new Size(108, 33);
+            button1.TabIndex = 6;
+            button1.Text = "Sampling";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(14, 25);
+            label2.Name = "label2";
+            label2.Size = new Size(114, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Next Sampling Time";
+            label2.Click += label2_Click;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(button2);
+            groupBox2.Controls.Add(textBox1);
+            groupBox2.Controls.Add(label1);
+            groupBox2.Location = new Point(12, 114);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(254, 105);
+            groupBox2.TabIndex = 0;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Logging";
+            groupBox2.Enter += groupBox2_Enter;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(147, 25);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(91, 23);
+            textBox2.TabIndex = 6;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(textBox3);
+            groupBox3.Location = new Point(292, 12);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(146, 207);
+            groupBox3.TabIndex = 5;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Sensor Values";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(6, 22);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(134, 179);
+            textBox3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(454, 231);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox3;
+        private Label label4;
+        private TextBox textBox4;
+        private GroupBox groupBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private GroupBox groupBox2;
+        private GroupBox groupBox3;
+    }
+}
---
+++ Final_Project_Dev_Ahmadi/Final_Project_Dev/CSV_FORM/Form1.cs
@@ -0,0 +1,106 @@
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+
+namespace CSV_FORM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //private void label1_Click(object sender, EventArgs e)
+        //{
+
+        //}
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    MessageBox.Show($"Hello {textBox1.Text}{textBox2.Text} , your email is: {textBox3.Text}, your phone is {textBox4.Text}");
+        //}
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var first_name = textBox1.Text;
+            //var last_name = textBox2.Text;
+            //var email = textBox3.Text;
+            //var phone = textBox4.Text;
+            string file = @".\test.csv";
+
+
+            //string file = @".\test_file.csv";
+
+            string sep = ",";
+            //string[] header = { "Input Value:" };
+            string[] data = { first_name };
+            StringBuilder s1 = new StringBuilder();
+
+            //s1.AppendLine(string.Join(sep, header));
+            s1.AppendLine(string.Join(sep, data));
+            try
+            {
+
+
+
+                File.AppendAllTextAsync(file, s1.ToString());
+                MessageBox.Show("Data Saved To CSV");
+                textBox1.Clear();
+
+
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Data couldn't be saved" + ex);
+
+            }
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
